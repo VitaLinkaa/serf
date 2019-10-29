2935718f00756307931dbe3a3a5793dd622addd3
Vita <Linkaa117@gmail.com>
2019-10-29 15:23:25 +0200

    slider

--- app/js/main.js
+++ app/js/main.js
@@ -15,4 +15,19 @@ $(document).ready(function () {
   });
   // склеиваем 2 слайдера
 
+  $('.serf__slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arr-left.svg" alt="arrow">',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arr-right.svg" alt="arrow">',
+    asNavFor: '.slider__map'
+  });
+
+  $('.slider__map').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor:'.serf__slider',
+    focusOnSelect: true
+  });
 });
